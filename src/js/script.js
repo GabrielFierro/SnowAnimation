6f634d736cbcba097dd49042fd0def9b673163ac
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-01-10 19:02:25 -0300

    add: add classes to display the snow flake

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 function createSnowFlake() {
 	const snow_flake = document.createElement("i");
+	snow_flake.classList.add("fas");
+	snow_flake.classList.add("fa-snowflake");
+	snow_flake.style.left = Math.random() * window.innerWidth + "px";
 
-	body.appendChild("snow_flake");
+	document.body.appendChild(snow_flake);
 }
